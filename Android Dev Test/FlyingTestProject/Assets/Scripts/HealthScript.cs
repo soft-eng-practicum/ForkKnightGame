6ee6b68e2304d3607460affa977a0e46e01b6572
dmfloyd <deronnefloyd@gmail.com>
2014-04-03 15:41:07 -0400

    Starting Unity2D Code
    Just universal from the tutorial we all have worked on at the start of
    the project. This should help get us started.

---
+++ Android Dev Test/FlyingTestProject/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+///<summary>
+/// Handle Hit points and damages
+/// </summary>
+
+public class HealthScript : MonoBehaviour {
+	/// <summary>
+	/// Total hitpoints
+	/// </summary>	
+
+	public int hp = 1;
+
+	///<summary>
+	/// Enemy or player?
+	/// </summary>
+
+	public bool isEnemy = true;
+
+	///<summary>
+	/// Inflicts damage and check if the object should be destoryed
+	/// </summary>
+	/// <param name="damageCount"></para>
+	public void Damage(int damageCount)
+	{
+				hp -= damageCount;
+
+				if (hp <= 0)
+		        {
+						//Dead!
+						Destroy(gameObject);
+				}
+		}
+
+	void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+				//Is this a shot?
+				ShotScript shot = otherCollider.gameObject.GetComponent<ShotScript>();
+				if (shot != null) {
+						//Avoid friendly fire
+						if (shot.isEnemyShot != isEnemy) {
+								Damage(shot.damage);
+
+								// Destroy the shot
+								Destroy(shot.gameObject); //Remember to always target the game object, otherwise you will remove the script
+						}
+				}
+		}
+
+
+}
---
+++ Android Dev Test/FlyingTestProject/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+///<summary>
+/// Player controller and behavior
+/// </summary>
+
+public class PlayerScript : MonoBehaviour {
+
+	///<summary>
+	/// 1 - The speed of the ship
+	/// </summary>
+	public Vector2 speed = new Vector2(50,50);
+
+	// 2 - Store the movement
+	private Vector2 movement;
+
+	void Update()
+	{
+				// 3 - Retrieve axis information
+				float inputX = Input.GetAxis ("Horizontal");
+				float inputY = Input.GetAxis ("Vertical");
+
+				// 4 - Movement per direction
+				movement = new Vector2 (
+			speed.x * inputX,
+			speed.y * inputY);
+
+		// 5 - Shooting
+		bool shoot = Input.GetButtonDown("Fire1");
+		shoot |= Input.GetButtonDown("Fire2");
+		// Careful: For Mac users, ctrl + arrow is a bad idea
+		
+		if (shoot)
+		{
+			WeaponScript weapon = GetComponent<WeaponScript>();
+			if (weapon != null)
+			{
+				// false because the player is not an enemy
+				weapon.Attack(false);
+			}
+		}
+
+
+
+		}
+
+	void FixedUpdate()
+	{
+				// 5 - Move the game object
+				rigidbody2D.velocity = movement;
+
+		}
+}
+
