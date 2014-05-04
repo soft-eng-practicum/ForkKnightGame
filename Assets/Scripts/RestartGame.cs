686efb81c9c80c5f77704406acda4b407b1e4b82
Jennifer <oashiru@emory.edu>
2014-05-04 18:31:15 -0400

    Added Restart Button to Game Over
    Added Restart Button to Game Over

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartGame : MonoBehaviour {
+
+		private GUISkin skin;
+		
+		void Start()
+		{
+			// Load a skin for the buttons
+			skin = Resources.Load("RestartSkin") as GUISkin;
+		}
+		
+		void OnGUI()
+		{
+			const int buttonWidth = 154;
+			const int buttonHeight = 77;
+			
+			// Set the skin to use
+			GUI.skin = skin;
+			
+			// Draw a button to start the game
+			if (GUI.Button(
+				// Center in X, 2/3 of the height in Y
+				new Rect(550, 320, buttonWidth, buttonHeight),
+				""
+				))
+			{
+				// On Click, load the first level.
+				Application.LoadLevel("Stage1"); // "Stage1" is the scene name
+			}
+		}
+	}
